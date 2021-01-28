429a7e3d9fb2315b61c573e42a33377431c5c21d
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-28 14:04:39 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+  let configObject = {
+    method: "POST", 
+    headers: {
+      "Content_Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name,
+      email
+    })
+  }
+  return fetch("http://localhost:3000/users", configObject)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(object) {
+    document.body.innerHTML = `<h1>${object["id"]}</h1>`
+  })
+  .catch(function(error) {
+    document.body.innerHTML = error.message
+  })
+}
